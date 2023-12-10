6f3a01b86c2a1023adf30b70751a8ad26fce6302
takaaa220 <taaaaaaaaaa220@gmail.com>
2023-12-10 17:46:33 +0900

    setup

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,26 +1,23 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  vscode.window.showInformationMessage("Hello World from go-test-name-finder!");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "go-test-name-finder" is now active!');
+  let disposable = vscode.commands.registerCommand("extension.findTest", () => {
+    // TODO: 選択された位置の行数、カラム数(開始、終了)、ファイル名を取得する
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('go-test-name-finder.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from go-test-name-finder!');
-	});
+    const document = editor.document;
+    const selection = editor.selection;
+    const filePath = document.fileName;
 
-	context.subscriptions.push(disposable);
+    vscode.window.showInformationMessage(filePath);
+  });
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
