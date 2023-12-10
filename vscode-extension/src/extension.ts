c0b657f33f8dd1c97984bb35cc86e618ab4b9c67
takaaa220 <taaaaaaaaaa220@gmail.com>
2023-12-10 18:44:35 +0900

    Implement vscode extension for go subtest executor

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,23 +1,91 @@
 import * as vscode from "vscode";
+import { execSync } from "child_process";
+import { join } from "path";
 
 export function activate(context: vscode.ExtensionContext) {
-  vscode.window.showInformationMessage("Hello World from go-test-name-finder!");
+  let disposable = vscode.commands.registerCommand(
+    "go-test-executor.exec",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
 
-  let disposable = vscode.commands.registerCommand("extension.findTest", () => {
-    // TODO: 選択された位置の行数、カラム数(開始、終了)、ファイル名を取得する
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return;
+      const document = editor.document;
+      const filePath = document.fileName;
+      const selection = editor.selection;
+
+      const test = await getTest(context, filePath, selection).catch(() => {
+        // TODO: output error on development environment
+        // console.error(error)
+        return undefined;
+      });
+
+      if (!test) {
+        vscode.window.showErrorMessage("no tests found");
+        return;
+      }
+
+      const { functionName, subTestName } = test;
+
+      // see:
+      // - https://github.com/golang/vscode-go/blob/d6fb20289a8484e57dc4fa21a2f44094de7f1a5b/src/goRunTestCodelens.ts#L134-L138
+      // - https://github.com/golang/vscode-go/blob/master/src/goTest.ts#L234-L246
+      if (subTestName) {
+        vscode.window.showInformationMessage(
+          `Start to run "${subTestName}" in "${functionName}"`
+        );
+
+        vscode.commands.executeCommand("go.subtest.cursor", {
+          functionName,
+          subTestName,
+        });
+
+        return;
+      }
+
+      vscode.window.showInformationMessage(
+        `Start to run tests in "${functionName}"`
+      );
+      vscode.commands.executeCommand("go.test.cursor", {
+        functionName,
+      });
     }
-
-    const document = editor.document;
-    const selection = editor.selection;
-    const filePath = document.fileName;
-
-    vscode.window.showInformationMessage(filePath);
-  });
+  );
 
   context.subscriptions.push(disposable);
+}
+
+async function getTest(
+  context: vscode.ExtensionContext,
+  filePath: string,
+  selection: vscode.Selection
+): Promise<{ functionName: string; subTestName?: string }> {
+  return new Promise((resolve, reject) => {
+    try {
+      const binaryPath = join(
+        context.extensionPath,
+        "out/bin/test-name-finder"
+      );
+
+      // TODO: fix
+      const res = execSync(
+        `${binaryPath} -f ${filePath} -l ${selection.start.line + 1} -s ${
+          selection.start.character
+        } -e ${selection.end.character}`
+      );
+
+      const testName = res.toString();
+
+      const [functionName, subTestName] = testName
+        .split("/")
+        .map((s) => s.trim());
+
+      resolve({ functionName, subTestName });
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
 
 export function deactivate() {}
